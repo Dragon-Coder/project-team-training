1c2b4ea1c61597b6bc6b660643b47966492c88b1
LiefJamesCain <lief.cain@gmail.com>
2018-06-19 17:05:20 -0700

    alternate attempt at H2 change

--- script.js
+++ script.js
@@ -2,4 +2,7 @@
 var myHeading = document.querySelector('h2');
 myHeading.textContent = 'Hello World! I know Javascript!';
 
-// this is a change
+
+// this is another attempt at the same code above. HTML code needs 'id=LiefH2'
+var changeH2 = document.getElementbyId('LiefH2');
+changeH2.textContent = 'Hello World! I know Javascript!';
