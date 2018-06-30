0d7ff9695fdad85b387ed2624cf133ad845fb3ad
LiefJamesCain <lief.cain@gmail.com>
2018-06-29 19:22:32 -0700

    change js h2 function

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// this should change any text tagged as an h2
-var changeH2 = document.querySelector('h2');
-changeH2.textContent = 'Hello World! I know Javascript!';
-
 
 // this is another attempt at the same code above. HTML code needs 'id=LiefH2'
-var changeH2 = document.getElementbyId('LiefH2');
+var changeH2 = document.getElementbyId('liefH2');
 changeH2.textContent = 'Hello World! I know Javascript!';
+
+$("button").click(function () {
+    $("liefH2").toggle(500);
+});
