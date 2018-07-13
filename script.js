b5e55eed512bbec7b0499f4599e3eb9b0027273f
Lief Cain <lief.cain@dealerspike.com>
2018-07-13 08:55:00 -0700

    removing redundant code

--- script.js
+++ script.js
@@ -7,6 +7,5 @@ changeH2.textContent = 'Hello World! I know Javascript!';
 $("button").click(function () {
     $("h2").toggle(500);
 });
-var changeH2 = document.getElementbyId('liefsH2');
-changeH2.querySelector("liefsH2").innerHTML = 'Hello World! I know Javascript!';
+
 
